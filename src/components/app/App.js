6ed9a09d308fb5915f15e58735d09e9b2074af9a
Johny Xu <ahstuxq@163.com>
2021-01-02 11:53:15 +1100

    remove container

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,18 +10,9 @@ import Header from '../header';
 import Home from '../home';
 import ProductPage from '../product-page';
 import CheckoutPage from '../checkout';
-import styled from 'styled-components';
 
 const store = configureStore();
 const history = createBrowserHistory();
-
-const Container = styled.div`
-  width: 100%;
-  padding-right: 1rem;
-  padding-left: 1rem;
-  margin-right: auto;
-  margin-left: auto;
-`;
 
 export default function App() {
   return (
